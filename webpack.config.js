ea616866076a0d0d5f3332212ed6ea8360bcce25
haben mehar <habenmehari71@gmail.com>
2023-06-28 16:15:22 +0300

    webpack config added and linter congigurations setup added

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo App',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    // publicPath: '/',
+  },
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    // runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
