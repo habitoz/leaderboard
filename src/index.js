54b9cc30e74efd65d6cf5cae1b06e9ffd1eab54e
haben mehar <habenmehari71@gmail.com>
2023-07-01 14:23:34 +0300

    api integration done for fetching and saving new scores

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './style.css';
+import {
+  createNewGame, saveScore, fetchScores, loadToDOM,
+} from './scripts/utils.js';
+
+const form = document.getElementById('form');
+const playerName = document.getElementById('player-name');
+const playerScore = document.getElementById('player-score');
+const playersList = document.getElementById('players-list');
+const refreshBtn = document.getElementById('refresh-btn');
+
+let game = localStorage.getItem('game');
+if (!game) {
+  game = createNewGame('EPL-RESULT');
+}
+
+let scores = [];
+const fetchAndLoad = async () => {
+  scores = await fetchScores(game);
+  if (scores.length) loadToDOM(playersList, scores);
+};
+fetchAndLoad();
+refreshBtn.addEventListener('click', () => {
+  fetchAndLoad();
+});
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const saveData = async () => {
+    await saveScore(game, { user: playerName.value, score: +playerScore.value });
+    await fetchAndLoad();
+  };
+  saveData();
+});
